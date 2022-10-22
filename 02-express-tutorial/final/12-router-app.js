13ac763290f1aaa63267b3ce767cde997eaf3816
moh1idris <113855948+moh1idris@users.noreply.github.com>
2022-10-22 11:32:19 +0530

    commets-Fixed

--- 02-express-tutorial/final/12-router-app.js
+++ 02-express-tutorial/final/12-router-app.js
@@ -15,5 +15,5 @@ app.use('/api/people', people)
 app.use('/login', auth)
 
 app.listen(5000, () => {
-  console.log('Server is listening on port 5000....')
+  console.log('Server is listening on port 5000....')                    //--> here we will sync the routes
 })
